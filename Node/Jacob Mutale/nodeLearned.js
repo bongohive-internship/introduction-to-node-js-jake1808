c9c063877e28b25d19663e871fa9c54e155a1790
jake1808 <51802968+jake1808@users.noreply.github.com>
2019-06-14 20:22:02 +0300

    Add files via upload

---
+++ Node/Jacob Mutale/nodeLearned.js
@@ -0,0 +1,9 @@
+function Age(){
+          const name = "Jacob";
+          const otherName = "Mutale";
+          const earthBornDate = "200";
+          const yearToDay = "2019";
+          let earthAge = earthBornDate - yearToDay;
+          console.log("Am " + name + " " + otherName + " and I live on earth which I think is"+earthAge );
+}
+Age();
